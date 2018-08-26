80a597bc6ce5377173cdad83acf3af5a4f9d129d
Francesco Losciale <francesco.losciale@gmail.com>
2018-08-26 04:55:37 +0200

    Seek a particular book in a collection - RED

--- src/test/java/BookCollectionTest.java
+++ src/test/java/BookCollectionTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BookCollectionTest {
@@ -10,6 +11,16 @@ public class BookCollectionTest {
         BookCollection bookCollection = new BookCollection();
         bookCollection.add(book);
         assertTrue(bookCollection.contains(book));
+    }
+
+
+    @Test
+    public void testSeekBookInCollection() {
+        String book = "test";
+        BookCollection bookCollection = new BookCollection();
+        bookCollection.add(book);
+        String result = bookCollection.seek(book);
+        assertEquals(result, book);
     }
 
 }
