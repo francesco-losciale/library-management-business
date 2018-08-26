edede8968ff0f839427350b59658ce05d93b1a34
Francesco Losciale <francesco.losciale@gmail.com>
2018-08-26 18:10:16 +0200

    Make a book out of order - RED

--- src/main/java/BookCollection.java
+++ src/main/java/BookCollection.java
@@ -22,6 +22,15 @@ public class BookCollection {
         return null;
     }
 
+    public void move(Book book, BookCollection bookCollection) {
+        final boolean remove = this.collection.remove(book);
+        if (remove) {
+            bookCollection.add(book);
+        } else {
+            throw new RuntimeException("No occurrence of " + book + " was found");
+        }
+    }
+
     public static Book seek(Book book, BookCollection... collections) {
         for (BookCollection bookCollection : collections) {
             if (bookCollection.seek(book) != null) {
--- src/test/java/BookCollectionTest.java
+++ src/test/java/BookCollectionTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BookCollectionTest {
@@ -15,7 +16,6 @@ public class BookCollectionTest {
         bookCollection.add(book);
         assertTrue(bookCollection.contains(book));
     }
-
 
     @Test
     public void testSeekBookInCollection() {
@@ -42,4 +42,29 @@ public class BookCollectionTest {
         assertEquals(result, book);
     }
 
+    @Test
+    public void testMoveBookToDifferentCollection() {
+        Book book = new Book();
+        book.setTitle("test");
+        book.setAuthor("test");
+        book.setIsbn("test");
+        BookCollection bookCollection1 = new BookCollection();
+        bookCollection1.add(book);
+        BookCollection bookCollection2 = new BookCollection();
+        bookCollection1.move(book, bookCollection2);
+        assertFalse(bookCollection1.contains(book));
+        assertTrue(bookCollection2.contains(book));
+    }
+
+    @Test
+    public void testMakeBookOutOfOrder() {
+        Book book = new Book();
+        book.setTitle("test");
+        book.setAuthor("test");
+        book.setIsbn("test");
+        BookCollection bookCollection = new BookCollection();
+        bookCollection.add(book);
+        bookCollection.outOfOrder(book);
+        assertTrue(book.isOutOfOrder());
+    }
 }
