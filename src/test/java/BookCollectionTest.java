3ce4e0e8e334434915b9a5e6da23e3a492311f2e
Francesco Losciale <francesco.losciale@gmail.com>
2018-08-26 04:33:28 +0200

    Collect some books together - RED

---
+++ src/test/java/BookCollectionTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BookCollectionTest {
+
+    @Test
+    public void testCollectBooksTogheter() {
+        String book = "test";
+        List<String> collection = new ArrayList<String>();
+        assertTrue(collection.contains(book));
+    }
+
+}
